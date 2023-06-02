9e1b3098ed62425f3d3c9dfc0b4525f351d861ff
2gechan <7532602@gmail.com>
2023-06-02 16:38:35 +0900

    team_pjt

---
+++ bus/index.js
@@ -0,0 +1,7 @@
+const showBar = function () {
+  document.getElementById("list").style.display = "block";
+};
+
+const hideBar = function () {
+  document.getElementById("list").style.display = "none";
+};
