fc05d55d5d588f280c05c741a45fa510c0449e6a
2gechan <7532602@gmail.com>
2023-06-08 16:42:51 +0900

    bus_pjt

--- bus/index.js
+++
@@ -1,7 +0,0 @@
-const showBar = function () {
-  document.getElementById("list").style.display = "block";
-};
-
-const hideBar = function () {
-  document.getElementById("list").style.display = "none";
-};
